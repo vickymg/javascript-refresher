3204254159209c5c8a1c4a5f4fc2723514ee8ffc
Vicky Gray <vickygray26@hotmail.com>
2016-05-04 23:45:54 +0100

    string to number

--- questions.js
+++ questions.js
@@ -70,7 +70,7 @@ var repeatElements = function(array) {
 }
 
 var stringToNumber = function(string) {
-  return 'Write your method here';
+  return parseInt(string);
 }
 
 var calculateAverage = function(array) {
